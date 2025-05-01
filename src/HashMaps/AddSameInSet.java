0c8d211a6e64bda65bb879a47d6fb554a729e14f
UDITA <usasinghudita2225@gmail.com>
2025-05-01 14:38:39 +0530

    20 April

--- src/HashMaps/AddSameInSet.java
+++ src/HashMaps/AddSameInSet.java
@@ -1,4 +1,18 @@
 package HashMaps;
-
+import java.util.HashSet;
 public class AddSameInSet {
+    public static void main(String[] args) {
+//        HashSet<Integer> set = new HashSet<>();
+//        Duplicacy is Not Allowed in HashSet
+//        Chahe Kitne Baar Bhi Add karlo same element, wo sirf ek baar hi add Hoga
+        HashSet<String> set =  new HashSet<>();
+        set.add("Udita");
+        set.add("Udita");
+        set.add("Udita");
+        set.add("Udita");
+        System.out.println(set);//[Udita]
+        System.out.println(set.size());//1
+        set.remove("Udita");
+        System.out.println(set +" "+ set.size());//[] 0
+    }
 }
