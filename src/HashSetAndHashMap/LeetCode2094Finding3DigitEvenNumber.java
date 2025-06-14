0623315d80e1b5c7babae4b6077866baa9854d0b
UDITA <usasinghudita2225@gmail.com>
2025-06-14 18:24:52 +0530

    20 April

--- src/HashSetAndHashMap/LeetCode2094Finding3DigitEvenNumber.java
+++ src/HashSetAndHashMap/LeetCode2094Finding3DigitEvenNumber.java
@@ -27,4 +27,89 @@ Constraints:
 0 <= digits[i] <= 9
 */
 public class LeetCode2094Finding3DigitEvenNumber {
+/* Sir's Method
+class Solution {
+    public int[] findEvenNumbers(int[] digits) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<digits.length; i++){
+            int key = digits[i];
+            if(map.containsKey(key)){
+                int freq = map.get(key);
+                map.put(key,freq+1);
+            }
+            else map.put(key,1);
+        }
+        for(int i=100; i<999; i+=2){
+            int x =i;
+            int c =x%10; x/=10;
+            int b =x%10; x/=10;
+            int a =x;
+            if(map.containsKey(a)){
+                int freq1 = map.get(a);
+                map.put(a, freq1-1);
+                if(freq1==1) map.remove(a);
+                if(map.containsKey(b)){
+                    int freq2 = map.get(b);
+                    map.put(b, freq2-1);
+                    if(freq2==1) map.remove(b);
+                    if(map.containsKey(c)){
+                        ans.add(i);
+                    }
+                    map.put(b,freq2);
+                }
+                map.put(a,freq1);
+            }
+        }
+        int[] arr = new int[ans.size()];
+        for(int i=0; i<ans.size(); i++){
+            arr[i] = ans.get(i);
+        }
+        Arrays.sort(arr);
+        return arr;
+    }
+}
+*/
+/* This is my code
+class Solution {
+    public int[] findEvenNumbers(int[] digits) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<digits.length; i++){
+            int key = digits[i];
+            if(map.containsKey(key)){
+                int freq = map.get(key);
+                map.put(key,freq+1);
+            }
+            else map.put(key,1);
+        }
+        for(int i=100; i<999; i+=2){
+            int x =i;
+            int c =x%10; x/=10;
+            int b =x%10; x/=10;
+            int a =x;
+            HashMap<Integer, Integer> temp =  new HashMap<>(map);
+            if(temp.containsKey(a) && temp.get(a)>=1){
+                int freq1 =  temp.get(a);
+                temp.put(a,freq1-1);
+                if(temp.containsKey(b)&& temp.get(b)>=1){
+                    int freq2 = temp.get(b);
+                    temp.put(b, freq2-1);
+                    if(temp.containsKey(c)&& temp.get(c)>=1){
+                        int freq3 = temp.get(c);
+                        temp.put(c, freq3-1);
+                        ans.add(i);
+                    }
+                }
+            }
+        }
+        int[] arr = new int[ans.size()];
+        for(int i=0; i<ans.size(); i++){
+            arr[i] = ans.get(i);
+        }
+        Arrays.sort(arr);
+        return arr;
+    }
+}
+*/
 }
