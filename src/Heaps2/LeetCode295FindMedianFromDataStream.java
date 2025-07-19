f448af6ea4de63ab365922f686182452b3f5400e
UDITA <usasinghudita2225@gmail.com>
2025-07-19 17:44:27 +0530

    20 April

--- src/Heaps2/LeetCode295FindMedianFromDataStream.java
+++ src/Heaps2/LeetCode295FindMedianFromDataStream.java
@@ -31,7 +31,79 @@ If all integer numbers from the stream are in the range [0, 100], how would you 
 If 99% of all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
 */
 public class LeetCode295FindMedianFromDataStream {
-/*
+/*Approach 3
+//TC O(nlogn) + O(n) ~ O(nlogn)
+//Best Method
+//Optimised Code
+class MedianFinder {
+    PriorityQueue<Integer> maxHeap;
+    PriorityQueue<Integer> minHeap;
+    public MedianFinder() {
+        maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        minHeap = new PriorityQueue<>();
+    }
+
+    public void addNum(int num) {
+        if(maxHeap.size()==0) maxHeap.add(num);
+        else{
+            if(maxHeap.peek()>num) maxHeap.add(num);
+            else minHeap.add(num);
+        }
+        //Balance The Heaps
+        if(maxHeap.size()==minHeap.size()+2){
+            int top = maxHeap.remove();
+            minHeap.add(top);
+        }
+        if(minHeap.size()==maxHeap.size()+2){
+            int top = minHeap.remove();
+            maxHeap.add(top);
+        }
+        // if(Math.abs(maxHeap.size()-minHeap.size())>1){
+        //         if(maxHeap.size()>minHeap.size()) minHeap.add(maxHeap.remove());
+        //         else maxHeap.add(minHeap.remove());
+        // }
+    }
+
+    public double findMedian() {
+    if(maxHeap.size()==minHeap.size())  return (maxHeap.peek()+minHeap.peek())/2.0;
+    if(maxHeap.size()>minHeap.size()) return maxHeap.peek();
+    else return minHeap.peek();
+    }
+}
+
+
+// Approach 2.
+//Brute Force
+//Better than previous one
+//Continuosly Sorting Using Insertion Sort
+//Time Complexity: -  O(n^2) Bad Time Complexity
+class MedianFinder {
+    List<Integer> arr;
+    public MedianFinder() {
+    arr = new ArrayList<>();   //This is a better habit
+    }
+    public void swap(int x, int y){
+        int temp = arr.get(x);
+        arr.set(x,arr.get(y));
+        arr.set(y,temp);
+    }
+    public void addNum(int num) {
+    arr.add(num);
+    int n = arr.size();
+    int i =n-1;
+    while(i>=1 && arr.get(i)<arr.get(i-1)){
+        swap(i,i-1);
+        i--;
+      }
+    }
+    public double findMedian() {
+    int n = arr.size();
+    if(n%2!=0) return arr.get(n/2);
+    else  return (arr.get(n/2 - 1) + arr.get(n/2))/2.0;
+    }
+}
+
+//Approach 1
 //Brute Force
 //Time Complexity: -  O(n^2logn) Bad Time Complexity
 class MedianFinder {
