c5949de66f9c5d8349bfd2c6f819e1d72bc2eb62
Udita2225 <usasinghudita2225@gmail.com>
2026-03-29 15:18:35 +0530

    29 March 2026

--- src/Graphs/LeetCode200NumberOfIslands.java
+++ src/Graphs/LeetCode200NumberOfIslands.java
@@ -7,7 +7,8 @@ premium lock icon
 Companies
 Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
 
-An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume
+all four edges of the grid are all surrounded by water.
 
 
 
@@ -110,7 +111,7 @@ public class LeetCode200NumberOfIslands {
 
 
 
-// This is my code .. It is accepted but prefer Sir's Code
+// This is my code .. It is accepted but prefer Sir's Code for variable names
 class Solution {
     public class Pair{
         int x;
