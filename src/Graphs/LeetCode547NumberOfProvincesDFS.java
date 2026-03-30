e017de857581f9da7deab9f50b820eb671392587
Udita2225 <usasinghudita2225@gmail.com>
2026-03-30 11:27:25 +0530

    29 March 2026

--- src/Graphs/LeetCode547NumberOfProvincesDFS.java
+++ src/Graphs/LeetCode547NumberOfProvincesDFS.java
@@ -1,4 +1,32 @@
 package Graphs;
 
 public class LeetCode547NumberOfProvincesDFS {
+    /* dfs
+    class Solution {
+        public void dfs(int idx, int[][] isConnected, boolean[] isVisited){
+            int n =  isConnected.length;
+            int[] arr =  isConnected[idx];
+            isVisited[idx] =  true;
+            for(int i=0; i<n; i++){
+                if(isConnected[idx][i] == 1 && isVisited[i]==false){
+                    isVisited[i] = true;
+                    dfs(i,isConnected,isVisited);
+                }
+            }
+        }
+        public int findCircleNum(int[][] isConnected) {
+            int n =  isConnected.length;
+            boolean[] isVisited =  new boolean[n];
+            int provinces = 0;
+            for(int i=0; i<n; i++){
+                if(isVisited[i]==false){
+                    dfs(i, isConnected, isVisited);
+                    provinces++;
+                }
+            }
+            return provinces;
+        }
+    }
+
+     */
 }
